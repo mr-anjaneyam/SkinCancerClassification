16aa92ea61acf80479684999500c4af7c600870b
Sri Anjaneyam <mr.anjaneyam@gmail.com>
2024-07-18 22:46:24 +0530

    disable right-cilck

--- script.js
+++ script.js
@@ -99,6 +99,25 @@ document.getElementById('close-contact-popup').addEventListener('click', () => {
     document.getElementById('contact-popup').classList.add('hidden');
 });
 
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function(e) {
+  if(e.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
+
 window.ondragstart = function() { return false; }
 
 
